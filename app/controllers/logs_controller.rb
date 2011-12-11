6b6b42b85012ae71243f45bfffca146e13c59303
hokiecanada <hokiecanada@hotmail.com>
2011-12-11 12:43:48 -0500

    .

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -7,10 +7,14 @@ class LogsController < ApplicationController
   
   def create
 	outlet = Outlet.find(params[:outlet_id])
+	o1 = Outlet.find(1).status.to_s()
+	o2 = Outlet.find(2).status.to_s()
+	o3 = Outlet.find(3).status.to_s()
+	
 	log = outlet.logs.create(params[:log])
 	log.time = DateTime.now
     if log.save
-		response = { :status =>  outlet.status }
+		response = { :status =>  (o1 + o2 + o3) }
 		render json: response.to_json()
 	else
 		response = { :status => 1, :time => Time.now }
